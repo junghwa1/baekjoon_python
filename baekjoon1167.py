862a9c8797568e1fe503f5a1cdb62c389cf52274
junghwa1 <57510050+junghwa1@users.noreply.github.com>
2023-06-25 15:40:15 +0900

    Add files via upload

--- baekjoon1167.py
+++ baekjoon1167.py
@@ -1,38 +1,38 @@
-from sys import stdin
+import sys
 
-class vertex():
-    def __init__(self,v):
-        self.edge=[]
-        self.v=v
-    
-    def insert_edge(self, v, dist):
-        self.edge.append([v,dist])
-
-def dfs(v):
-    print()
+sys.setrecursionlimit(10**6)
 
 
-input=stdin.readline
+
+def dfs(v,num):
+
+    for x, y in tree[v]:
+        if vertex_dist[x]==-1:
+            vertex_dist[x]=y+num
+            dfs(x,y+num)
+
+
+input=sys.stdin.readline
+
 
 V=int(input())
 
-vertex_set=[]
+tree = [[] for _ in range(V+1)]
+vertex_dist = [-1] * (V + 1)
 
-leaf_node=[]
 
 for i in range(1,V+1):
-    vertex_set.append(vertex(i))
-
-for i in range(0,V):
     arr=list(map(int,input().strip().split()))
-    arr=arr[1:-1]
-    if len(arr)==2:
-        leaf_node=[]
-    for j in range(1,len(arr),2):
-        vertex_set[i].insert_edge(arr[j-1],arr[j])
 
-Max=0
-sum=0
+    for j in range(1, len(arr) - 2, 2):
+        tree[arr[0]].append([arr[j], arr[j + 1]])
 
 
-print()
+vertex_dist[1]=0
+dfs(1,0)
+s=vertex_dist.index(max(vertex_dist))
+vertex_dist = [-1] * (V + 1)
+vertex_dist[s]=0
+dfs(s,0)
+
+print(max(vertex_dist))
